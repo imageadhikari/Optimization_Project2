20cf3920cd23f254e1e3b83193468d3c37ed32af
unknown <imageadhikari@gmail.com>
2024-10-29 14:38:57 -0400

    feat: project complete

--- optimization_methods.py
+++ optimization_methods.py
@@ -72,8 +72,8 @@ def quasi_newton_bfgs(f, grad_f, x0, delta1=1e-3, delta2=1e-3, max_iter=1000):
 def adam_optimizer(f, x0, grad, max_iters=1000, initial_learning_rate=0.001, delta1=1e-4, delta2=1e-4):
     x_k = x0
     fx_ls = [f(x_k)]
-    s = np.zeros_like(x_k)  # Initialize first moment vector
-    r = np.zeros_like(x_k)  # Initialize second moment vector
+    m = np.zeros_like(x_k)  # Initialize first moment vector
+    v = np.zeros_like(x_k)  # Initialize second moment vector
 
     rho1 = 0.9  # Decay rate for first moment
     rho2 = 0.999  # Decay rate for second moment
@@ -82,15 +82,15 @@ def adam_optimizer(f, x0, grad, max_iters=1000, initial_learning_rate=0.001, del
     for k in range(max_iters):
         g = grad(x_k)
 
-        s = rho1 * s + (1 - rho1) * g
-        r = rho2 * r + (1 - rho2) * (g ** 2)
+        m = rho1 * m + (1 - rho1) * g
+        v = rho2 * v + (1 - rho2) * (g ** 2)
 
-        s_hat = s / (1 - rho1 ** (k + 1))
-        r_hat = r / (1 - rho2 ** (k + 1))
+        m_hat = m / (1 - rho1 ** (k + 1))
+        v_hat = v / (1 - rho2 ** (k + 1))
 
         learning_rate = initial_learning_rate * np.sqrt(1 - rho2 ** (k + 1)) / (1 - rho1 ** (k + 1))
 
-        p_k = -learning_rate * s_hat / (np.sqrt(r_hat) + epsilon)
+        p_k = -learning_rate * m_hat / (np.sqrt(v_hat) + epsilon)
         x_k_next = x_k + p_k
 
         fx_next = f(x_k_next)
